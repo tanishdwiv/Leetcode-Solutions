d108a1044edd02e93575b4fb6a99afff533ae1d5
Tanish Dwivedi <147266495+tanishdwiv@users.noreply.github.com>
2025-07-05 18:56:13 +0530

    Time: 4 ms (34.1%), Space: 43.5 MB (93.53%) - LeetHub

--- 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.java
+++ 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.java
@@ -1,18 +1,35 @@
+// class Solution {
+//     public int[] finalPrices(int[] prices) {
+//         int[] arr=new int[prices.length];
+//         arr[prices.length-1]=prices[prices.length-1];
+//         for(int i=0;i<prices.length-1;i++){
+//             for(int j=i+1;j<prices.length;j++){
+//                 if(prices[j]<=prices[i]){
+//                     arr[i]=prices[i]-prices[j];
+//                     break;
+//                 }
+//                 else{
+//                     arr[i]=prices[i];
+//                 }
+//             }
+//         }
+//         return arr;
+//     }
+// }
 class Solution {
     public int[] finalPrices(int[] prices) {
-        int[] arr=new int[prices.length];
-        arr[prices.length-1]=prices[prices.length-1];
-        for(int i=0;i<prices.length-1;i++){
-            for(int j=i+1;j<prices.length;j++){
-                if(prices[j]<=prices[i]){
-                    arr[i]=prices[i]-prices[j];
-                    break;
-                }
-                else{
-                    arr[i]=prices[i];
-                }
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < prices.length; i++) {
+  
+            while (!stack.isEmpty() && prices[i] <= prices[stack.peek()]) {
+                int idx = stack.pop(); // Found the discount
+                prices[idx] = prices[idx] - prices[i]; 
             }
+        
+            stack.push(i);
         }
-        return arr;
+
+        return prices;
     }
 }
