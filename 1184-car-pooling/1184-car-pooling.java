ba7b273c7fde3bf2b157f079d4b06c61bc0cb564
Tanish Dwivedi <147266495+tanishdwiv@users.noreply.github.com>
2025-07-23 19:13:31 +0530

    Time: 1 ms (100%), Space: 44.1 MB (92.57%) - LeetHub

--- 1184-car-pooling/1184-car-pooling.java
+++ 1184-car-pooling/1184-car-pooling.java
@@ -1,20 +1,18 @@
 class Solution {
     public boolean carPooling(int[][] trips, int capacity) {
-        int[] timeline=new int[1001];
+        int[] data=new int[1001];
         for(int[] trip:trips){
             int passengers=trip[0];
             int from=trip[1];
             int to=trip[2];
 
-        timeline[from]=timeline[from]+passengers;
-        timeline[to]=timeline[to]-passengers;
-
-
-        }
+        data[from]=data[from]+passengers;
+        data[to]=data[to]-passengers;
+          }
       
         int mycurrent=0;
         for(int i=0;i<1000;i++){
-            mycurrent+=timeline[i];
+            mycurrent+=data[i];
             if(mycurrent>capacity){
                 return false;
             }
