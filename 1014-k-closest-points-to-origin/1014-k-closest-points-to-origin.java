cdfefdd5465e633f31e000aa41f804f32dac8331
Tanish Dwivedi <147266495+tanishdwiv@users.noreply.github.com>
2025-07-28 17:56:43 +0530

    Time: 30 ms (62.77%), Space: 50.5 MB (88.56%) - LeetHub

--- 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.java
+++ 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.java
@@ -1,24 +1,32 @@
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
-        // long x2=0;
-        // long y2=0;
-        // for(int i=0;i<points.length;i++){
-        //     long x1=points[i][0];
-        //     long y1=points[i][1];
-        //      long sqrt=Math.sqrt(((x1-x2)*(x1-x2))+((y1-y2)*(y1-y2)));
-        // }
-        // PriorityQueue<Integer> minheap=new PriorityQueue<>();
-        // minheap.offer(sqrt);
+       
 
         // Sort the entire array using a custom comparator.
         // The comparator calculates the squared Euclidean distance for two points (a and b)
         // and sorts them based on that distance.
-        Arrays.sort(points, (a, b) -> 
-            (a[0] * a[0] + a[1] * a[1]) - (b[0] * b[0] + b[1] * b[1])
-        );
+        // Arrays.sort(points, (a, b) -> 
+        //     (a[0] * a[0] + a[1] * a[1]) - (b[0] * b[0] + b[1] * b[1])
+        // );
         
         // Return the first k elements from the sorted array.
-        return Arrays.copyOfRange(points, 0, k);
+        // return Arrays.copyOfRange(points, 0, k);
 
+//maxheap i have taken as it will store the maximum distance points array from origin 
+        PriorityQueue<int[]> maxheap=new PriorityQueue<>((a,b)->(b[0]*b[0]+b[1]*b[1])-(a[0]*a[0]+a[1]*a[1]));
+
+for(int[] point:points){
+    maxheap.offer(point);
+    //aur hum jbtk maxheap se nikalte rhenge jb tk k points na ho jayen anadar
+    if(maxheap.size()>k){
+    maxheap.poll();
+}
+}
+
+int[][] answer=new int[k][2];
+for(int i=0;i<k;i++){
+    answer[i]=maxheap.poll();
+}
+return answer;
     }
 }
