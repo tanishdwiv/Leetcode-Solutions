05fd90406050b512351d068d4db6a41618b5a69f
Tanish Dwivedi <147266495+tanishdwiv@users.noreply.github.com>
2025-08-14 13:55:19 +0530

    Time: 83 ms (15.75%), Space: 60.3 MB (48.74%) - LeetHub

--- 1886-minimum-limit-of-balls-in-a-bag/1886-minimum-limit-of-balls-in-a-bag.java
+++ 1886-minimum-limit-of-balls-in-a-bag/1886-minimum-limit-of-balls-in-a-bag.java
@@ -1,28 +1,33 @@
 class Solution {
     public int minimumSize(int[] nums, int maxOperations) {
-        int left = 1;
-        int  right = 1000_000_000;
-        int ans = right;
-
-        while (left <= right) {
-            int mid = (left + right) / 2;
-            int ops = 0;
-
-            for (int n : nums) {
-                ops += (n - 1) / mid;
-                if (ops > maxOperations) {
-                    break;
-                }
-            }
-
-            if (ops <= maxOperations) {
-                ans = mid;
-                right = mid - 1;
-            } else {
-                left = mid + 1;
-            }
+        int low = 1;
+        int high = 0;
+        for (int num : nums) {
+            high = Math.max(high, num);
         }
 
+        int ans = high;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (canMake(nums, mid, maxOperations)) {
+                ans = mid;
+                high = mid - 1; // try smaller penalty
+            } else {
+                low = mid + 1; // penalty too small, try bigger
+            }
+        }
         return ans;
+    }
+
+    private boolean canMake(int[] nums, int X, int maxOperations) {
+        int totalCuts = 0;
+        for (int num : nums) {
+            if (num > X) {
+                int piecesNeeded = (int) Math.ceil((double) num / X);
+                totalCuts += piecesNeeded - 1;
+                if (totalCuts > maxOperations) return false;
+            }
+        }
+        return true;
     }
 }
