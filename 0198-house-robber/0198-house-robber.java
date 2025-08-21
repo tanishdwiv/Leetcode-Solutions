24f641cfcecb785d202558fbc8a04d4f44fa21c2
Tanish Dwivedi <147266495+tanishdwiv@users.noreply.github.com>
2025-08-21 22:49:51 +0530

    Time: 0 ms (100%), Space: 41.1 MB (46.04%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,31 +1,39 @@
 class Solution {
     public int rob(int[] nums) {
-    //     return recursion(nums, nums.length - 1);
+    int n=nums.length;
+    int memo[]=new int[n+1];
+     Arrays.fill(memo, -1);
+    return Loot(nums,n,memo);
+    }
+    
+        public  int Loot(int[] robber, int n, int[] memo)
+         { 
+          if (n <= 0) return 0;
+          if (memo[n] != -1) return memo[n]; 
+          int include = robber[n - 1] + Loot(robber, n - 2, memo); 
+          int exclude = Loot(robber, n - 1, memo);
+          memo[n] = Math.max(include, exclude);
+          return memo[n]; 
+          }
+    }
+
+
+
+
+
+
+
+        //     return recursion(nums, nums.length - 1);
     // }
 
     // private int recursion(int[] nums, int n) {
     //     if (n < 0) return 0; // Base case: no houses left to rob
     //     if (n == 0) return nums[0]; // Only one house to rob
 
-    //     // Two choices: rob the current house and move two steps back, or skip it and move one step back
     //     int robCurrent = nums[n] + recursion(nums, n - 2);
     //     int skipCurrent = recursion(nums, n - 1);
     //     return Math.max(robCurrent, skipCurrent); // Take the max of both choices
 //top down
-    // int n=nums.length;
-    // int memo[]=new int[n+1];
-    // return maxLootRec(nums,n,memo);
-    // }
-    
-    //     public  int maxLootRec(int[] hval, int n, int[] memo)
-    //      { 
-    //       if (n <= 0) return 0;
-    //       if (memo[n] != 0) return memo[n]; 
-    //       int includeCurrent = hval[n - 1] + maxLootRec(hval, n - 2, memo); 
-    //       int excludeCurrent = maxLootRec(hval, n - 1, memo);
-    //        memo[n] = Math.max(includeCurrent, excludeCurrent); return memo[n]; 
-    //       }
-    // }
 
     //     int n = nums.length;
     //     int[] memo = new int[n + 1]; // Initialize memo array
@@ -43,25 +51,25 @@ class Solution {
     //     return memo[n];
     // }
 
-//bottom up
+// //bottom up
    
-        if (nums.length == 0) return 0;
-        if (nums.length == 1) return nums[0];
+//         if (nums.length == 0) return 0;
+//         if (nums.length == 1) return nums[0];
 
-        // Initialize an array to store the maximum money that can be robbed up to each house
-        int[] dp = new int[nums.length];
-        dp[0] = nums[0];
-        dp[1] = Math.max(nums[0], nums[1]);
+//         // Initialize an array to store the maximum money that can be robbed up to each house
+//         int[] dp = new int[nums.length];
+//         dp[0] = nums[0];
+//         dp[1] = Math.max(nums[0], nums[1]);
 
-        for (int i = 2; i < nums.length; i++) {
-            // For each house, calculate the maximum money that can be robbed
-            dp[i] = Math.max(dp[i - 1], nums[i] + dp[i - 2]);
-        }
+//         for (int i = 2; i < nums.length; i++) {
+//             // For each house, calculate the maximum money that can be robbed
+//             dp[i] = Math.max(dp[i - 1], nums[i] + dp[i - 2]);
+//         }
 
-        // The last element in dp array will have the maximum money that can be robbed
-        return dp[nums.length - 1];
-    }
+//         // The last element in dp array will have the maximum money that can be robbed
+//         return dp[nums.length - 1];
+//     }
 
    
 
-}
+
