c7b5abe5032b63af5080459d5f65535297a0c3ea
Tanish Dwivedi <147266495+tanishdwiv@users.noreply.github.com>
2025-08-27 00:48:36 +0530

    Time: 0 ms (100%), Space: 42 MB (22.58%) - LeetHub

--- 1025-minimum-cost-for-tickets/1025-minimum-cost-for-tickets.java
+++ 1025-minimum-cost-for-tickets/1025-minimum-cost-for-tickets.java
@@ -1,28 +1,74 @@
+// class Solution {
+//     public int mincostTickets(int[] days, int[] costs) {
+//         return solve(days,costs,0);
+
+//     }
+//   public  int solve(int[] days,int[] costs,int currentindex){
+//         if(currentindex>days.length){
+//             return 0;
+//         }
+
+//         //if i buy one day pass
+//         int day1cost=costs[0]+solve(days,costs,currentindex+1);
+
+//         //if i buy 7day pass
+//         int nextdayafter7=currentindex;
+//         while(nextdayafter7<days.length && days[nextdayafter7]<days[currentindex]+7){
+//              nextdayafter7++;
+//         }
+//         int day7cost=costs[1]+solve(days,costs, nextdayafter7);
+
+//        //if i buy day 30 pass
+//         int nextdayafter30=currentindex;
+//         while(nextdayafter30<days.length && days[nextdayafter7]<days[currentindex]+30){
+//              nextdayafter30++;
+//         }
+//         int day30cost=costs[2]+solve(days,costs, nextdayafter30);
+
+
+//         return Math.min(day1cost,Math.min(day7cost,day30cost));
+
+//     }
+// }
+
+
+
+//memoization
 class Solution {
     public int mincostTickets(int[] days, int[] costs) {
-        Map<Integer, Integer> memo = new HashMap<>();
-        return minCost(0, days, costs, memo);
+        int[] dp=new int[days.length+1];
+        Arrays.fill(dp,-1);
+        return solve(days,costs,0,dp);
+
     }
-    private int minCost(int dayIndex, int[] days, int[] costs, Map<Integer, Integer> memo) {
-        if (dayIndex >= days.length) {
+  public  int solve(int[] days,int[] costs,int currentindex,int[] dp){
+        if(currentindex>=days.length){
             return 0;
         }
-        if (memo.containsKey(dayIndex)) {
-            return memo.get(dayIndex);
+
+        if(dp[currentindex]!=-1){
+            return dp[currentindex];
         }
-        int cost1Day = costs[0] + minCost(dayIndex + 1, days, costs, memo);
-        int nextDayFor7DayPass = dayIndex;
-        while (nextDayFor7DayPass < days.length && days[nextDayFor7DayPass] < days[dayIndex] + 7) {
-            nextDayFor7DayPass++;
+
+        //if i buy one day pass
+        int day1cost=costs[0]+solve(days,costs,currentindex+1,dp);
+
+        //if i buy 7day pass
+        int nextdayafter7=currentindex;
+        while(nextdayafter7<days.length && days[nextdayafter7]<days[currentindex]+7){
+             nextdayafter7++;
         }
-        int cost7Day = costs[1] + minCost(nextDayFor7DayPass, days, costs, memo);
-   int nextDayFor30DayPass = dayIndex;
-        while (nextDayFor30DayPass < days.length && days[nextDayFor30DayPass] < days[dayIndex] + 30) {
-            nextDayFor30DayPass++;
+        int day7cost=costs[1]+solve(days,costs, nextdayafter7,dp);
+
+       //if i buy day 30 pass
+        int nextdayafter30=currentindex;
+        while(nextdayafter30<days.length && days[nextdayafter30]<days[currentindex]+30){
+             nextdayafter30++;
         }
-        int cost30Day = costs[2] + minCost(nextDayFor30DayPass, days, costs, memo);
-        int minCost = Math.min(cost1Day, Math.min(cost7Day, cost30Day));
-        memo.put(dayIndex, minCost);
-        return minCost;
+        int day30cost=costs[2]+solve(days,costs, nextdayafter30,dp);
+
+
+        return dp[currentindex]=Math.min(day1cost,Math.min(day7cost,day30cost));
+
     }
 }
