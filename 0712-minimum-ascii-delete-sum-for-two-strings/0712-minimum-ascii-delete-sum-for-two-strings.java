ab55f23ef18285d9c48f7c6e41316c0b35d8edf2
Tanish Dwivedi <147266495+tanishdwiv@users.noreply.github.com>
2025-08-29 00:55:43 +0530

    Time: 32 ms (9.93%), Space: 44.8 MB (67.49%) - LeetHub

---
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int minimumDeleteSum(String s1, String s2) {
+        int[][] memo = new int[s1.length()][s2.length()];
+        for (int[] row : memo) {
+            Arrays.fill(row, -1); // -1 signifies "not yet computed".
+        }
+        return solve(s1, 0, s2, 0,memo);
+    }
+    private int solve(String s1, int i, String s2, int j,int[][] memo) {
+        // Base Case 1: If s1 is exhausted, we must delete the rest of s2.
+        if (i == s1.length()) {
+            int remainingSum = 0;
+            for (int k = j; k < s2.length(); k++) {
+                remainingSum += s2.charAt(k);
+            }
+            return remainingSum;
+        }
+
+        // Base Case 2: If s2 is exhausted, we must delete the rest of s1.
+        if (j == s2.length()) {
+            int remainingSum = 0;
+            for (int k = i; k < s1.length(); k++) {
+                remainingSum += s1.charAt(k);
+            }
+            return remainingSum;
+        }
+
+         if (memo[i][j] != -1) {
+            return memo[i][j];
+        }
+
+        if (s1.charAt(i) == s2.charAt(j)) {
+            return solve(s1, i + 1, s2, j + 1,memo);
+        } else {
+        
+            int deleteFromS1 = s1.charAt(i) + solve(s1, i + 1, s2, j,memo);
+            int deleteFromS2 = s2.charAt(j) + solve(s1, i, s2, j + 1,memo);
+            
+            return memo[i][j]=Math.min(deleteFromS1, deleteFromS2);
+        }
+    }
+}
