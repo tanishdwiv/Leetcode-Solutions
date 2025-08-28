109b650004e36db2472c0192413a9aa8fcde963c
Tanish Dwivedi <147266495+tanishdwiv@users.noreply.github.com>
2025-08-28 16:13:21 +0530

    Time: 34 ms (15.15%), Space: 46.1 MB (6.54%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -8,20 +8,24 @@ class Solution {
         }
  int result = solve(coins, 0, amount, memo);
 
-        return (result >= 1_000_000_000) ? -1 : result;
+        return (result >=Integer.MAX_VALUE) ? -1 : result;
     }
  private int solve(int[] coins, int index, int amount, int[][] memo) {
      if (amount == 0) {
             return 0;
         }
  if (index >= coins.length || amount < 0) {
-            return 1_000_000_000; 
+            return Integer.MAX_VALUE; 
         }
         if (memo[index][amount] != -1) {
             return memo[index][amount];
         }  
  int dontTake = solve(coins, index + 1, amount, memo);
-  int take = 1 + solve(coins, index, amount - coins[index], memo);
+  int take = Integer.MAX_VALUE;
+        int takeResult = solve(coins, index, amount - coins[index], memo);
+        if (takeResult != Integer.MAX_VALUE) {
+            take = 1 + takeResult;
+        }
  
         return memo[index][amount] = Math.min(take, dontTake);
     }
