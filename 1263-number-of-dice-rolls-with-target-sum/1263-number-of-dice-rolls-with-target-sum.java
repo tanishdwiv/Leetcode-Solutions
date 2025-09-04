93eadbd6483d4bc9df4e6daa41b69398ec2b8ab5
Tanish Dwivedi <147266495+tanishdwiv@users.noreply.github.com>
2025-09-04 17:20:36 +0530

    Time: 9 ms (91.21%), Space: 41.7 MB (87.29%) - LeetHub

---
+++ 1263-number-of-dice-rolls-with-target-sum/1263-number-of-dice-rolls-with-target-sum.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int numRollsToTarget(int n, int k, int target) {
+        int[][] memo=new int[n+1][target+1];
+        for(int[] row:memo){
+            Arrays.fill(row,-1);
+           
+        }
+        return solve(n,k,target,memo);
+    }
+    int solve(int n,int k,int target,int[][] memo){
+       
+        if(n==0 && target==0){
+            return 1 ;
+        }
+         if(n==0){
+            return 0;
+        }
+        if(memo[n][target]!=-1){
+            return memo[n][target];
+        }
+        int ans=0;
+       int mod = 1_000_000_007;
+        for(int i=1;i<=k;i++){
+            if(target>=i){
+            ans=(ans+solve(n-1,k,target-i,memo))%mod;
+            }
+        }
+        return memo[n][target]=ans;
+    }
+}
