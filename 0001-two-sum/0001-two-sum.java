a49fd17db638f7e25e20a33f3b22b19af1f9f83d
Tanish Dwivedi <147266495+tanishdwiv@users.noreply.github.com>
2025-12-19 00:55:24 +0530

    Time: 8 ms (45.63%), Space: 47.1 MB (28.57%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,35 +1,35 @@
+import java.util.*;
+
 class Solution {
     public int[] twoSum(int[] nums, int target) {
 
-        int i = 0,j = nums.length -1;
-        int[]temp = nums.clone();
-        Arrays.sort(temp);
-        while(temp[i] + temp[j] != target){
-            int sum = temp[i] + temp[j];
-            if( sum < target){
-                i++;
-            }
-            else {
-                j--;
-            }
-        }
-        int a1 = 0,a2 = 0;
-        // setting a1 and a2
-        for(int ii = 0 ; ii < nums.length;ii++){
-            if(nums[ii] == temp[i]){
-                a1 = ii;
-                break;  
-            }
+        // Step 1: store value + original index
+        int[][] arr = new int[nums.length][2];
+        for (int i = 0; i < nums.length; i++) {
+            arr[i][0] = nums[i]; // value
+            arr[i][1] = i;       // original index
         }
 
-        for(int ii = 0 ; ii < nums.length;ii++){
-            if(nums[ii] == temp[j] && a1!=ii){
-                a2 = ii;
-            }
+        // Step 2: sort by value
+        Arrays.sort(arr, (a, b) -> a[0] - b[0]);
 
+        // Step 3: two pointers
+        int left = 0, right = nums.length - 1;
+
+        while (left < right) {
+            int sum = arr[left][0] + arr[right][0];
+
+            if (sum == target) {
+                return new int[]{
+                    arr[left][1],
+                    arr[right][1]
+                };
+            } else if (sum < target) {
+                left++;
+            } else {
+                right--;
+            }
         }
-        int[] ans = {a1,a2};
-        return ans;
-        
+        return new int[]{};
     }
 }
