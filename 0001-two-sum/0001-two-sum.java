31dec20f01f5aeff1aceba2d9052286eb50978eb
Tanish Dwivedi <147266495+tanishdwiv@users.noreply.github.com>
2025-12-25 16:22:59 +0530

    Time: 8 ms (46.09%), Space: 46.9 MB (43%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -6,14 +6,10 @@ class Solution {
         // Step 1: store value + original index
         int[][] arr = new int[nums.length][2];
         for (int i = 0; i < nums.length; i++) {
-            arr[i][0] = nums[i]; // value
-            arr[i][1] = i;       // original index
+            arr[i][0] = nums[i]; 
+            arr[i][1] = i;       
         }
-
-        // Step 2: sort by value
         Arrays.sort(arr, (a, b) -> a[0] - b[0]);
-
-        // Step 3: two pointers
         int left = 0, right = nums.length - 1;
 
         while (left < right) {
