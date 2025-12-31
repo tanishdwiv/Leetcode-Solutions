004a6e6f0445ae817001cad62699594515fe4535
Tanish Dwivedi <147266495+tanishdwiv@users.noreply.github.com>
2025-12-31 10:23:24 +0530

    Time: 6 ms (67.08%), Space: 46.4 MB (45.42%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,26 +1,44 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
         //using sliding window
-        int n=s.length();
-        HashSet<Character> set=new HashSet<>();
-        int left=0;
-        int right=0;
-        int max=0;
-        while(right<n){
-            //iam checking if it does not contains in the set ,if it doesnt than i will add that character and increase right pointer
-            if(!set.contains(s.charAt(right))){
-                set.add(s.charAt(right));
-                right++; 
-            max=Math.max(max,right-left);
-            }
-            else{
-                //if it contains in the set....remove the left chararcter and decrese the window
-                set.remove(s.charAt(left));
-                left++;
+        // int n=s.length();
+        // HashSet<Character> set=new HashSet<>();
+        // int left=0;
+        // int right=0;
+        // int max=0;
+        // while(right<n){
+        //     //iam checking if it does not contains in the set ,if it doesnt than i will add that character and increase right pointer
+        //     if(!set.contains(s.charAt(right))){
+        //         set.add(s.charAt(right));
+        //         right++; 
+        //     max=Math.max(max,right-left);
+        //     }
+        //     else{
+        //         //if it contains in the set....remove the left chararcter and decrese the window
+        //         set.remove(s.charAt(left));
+        //         left++;
+        //  }
+        // }
+        // return max;
 
-            }
-        }
-        return max;
-       
+
+
+int left=0;
+int right=0;
+int n=s.length();
+int max=0;
+HashSet<Character> set=new HashSet<>();
+while(right<n){
+    if(!set.contains(s.charAt(right))){
+        set.add(s.charAt(right));
+        right++;
+         max=Math.max(max,right-left);
+    }
+    else{
+        set.remove(s.charAt(left));
+        left++;
+    }
+}
+return max;    
     }
 }
