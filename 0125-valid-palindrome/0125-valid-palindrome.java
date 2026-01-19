12774f6ca88cc812d7214bf4e5b456b9c51f69eb
Tanish Dwivedi <147266495+tanishdwiv@users.noreply.github.com>
2026-01-19 23:24:32 +0530

    Time: 15 ms (20.76%), Space: 46.7 MB (18.21%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,39 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        // StringBuilder sb = new StringBuilder();
+        // s = s.toLowerCase();
+        // for(int i = 0 ; i < s.length() ; i++){
+        //     char ch = s.charAt(i);
+        //     if((ch <= 122 && ch >= 97) || (ch <= 90 && ch >= 65 || (ch <= 57 && ch >= 48))){
+        //         sb.append(ch);
+        //     }
+        // }
+       
+        // int i = 0;
+        // int j = sb.length() -1;
+        // while(i<j){
+        //     char ch1 = sb.charAt(i);
+        //     char ch2 = sb.charAt(j);
+        //     if(ch1 != ch2)
+        //     return false;
+        //     i++;
+        //     j--;
+        // }
+        // return true;
+           s = s.toLowerCase().replaceAll("[^A-Za-z0-9]", "");
+
+        int start = 0;
+        int end = s.length()-1;
+
+        while(start<=end){
+
+            if(s.charAt(start) != s.charAt(end)){
+                return false;
+            }
+
+            start++;
+            end--;
+        }
+        return true;
+    }
+}
