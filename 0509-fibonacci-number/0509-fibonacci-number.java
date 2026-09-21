90a9b7c9a407dd537ae02eeb70385fc4be3f9400
Tanish Dwivedi <147266495+tanishdwiv@users.noreply.github.com>
2026-09-21 22:40:35 +0530

    Time: 0 ms (100%), Space: 42.2 MB (16.29%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,18 +1,35 @@
 class Solution {
-    public int fib(int n) {
+    // public int fib(int n) {
        
-        int dp[]=new int[n+1];
-        Arrays.fill(dp, -1);
-        return f(n,dp);
+    //     int dp[]=new int[n+1];
+    //     Arrays.fill(dp, -1);
+    //     return f(n,dp);
         
-    }
-    static int f(int n,int[]dp){
-         if(n<=1)
-        return n;
-        if(dp[n]!=-1){
-            return dp[n];
+    // }
+    // static int f(int n,int[]dp){
+    //      if(n<=1)
+    //     return n;
+    //     if(dp[n]!=-1){
+    //         return dp[n];
+    //     }
+    //     dp[n]=f(n-1,dp)+f(n-2,dp);
+    //     return dp[n];
+    // }
+      public int fib(int n) {
+
+        if (n <= 1) {
+            return n;
         }
-        dp[n]=f(n-1,dp)+f(n-2,dp);
+
+        int[] dp = new int[n + 1];
+
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
         return dp[n];
     }
 
